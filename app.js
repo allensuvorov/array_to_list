7b9b972c4dcf9b5d445357f8de249aa19d82b78f
Allen <allenvs@mail.ru>
2020-08-09 07:03:20 +0300

    added comments

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 function arrayToList(array) { 
     if (array.length == 0) { return null; };
-    
-    let head = {
-      value: array[0],
-      next: null
-    };
-    
-    let previous = head;
-  
-    for(let i = 1; i < array.length; i++) { 
-      previous.next = { 
-        value: array[i],
+    //creat list object with head data
+    let list = {
+        value: array[0],
         next: null
-      };
-      previous = previous.next;
-    }
-    return head;
-  }
-
-  console.log(arrayToList([10, 20, 30]));
+    };
+    let previous = list;
+    for(let i = 1; i < array.length; i++) { 
+        // create new object and assign it to next propery of the prev object
+        previous.next = { 
+            value: array[i],
+            next: null
+        };
+        // link previous to the new object
+        previous = previous.next;
+    };
+    return list;
+};
+console.log(arrayToList([10, 20, 30]));
